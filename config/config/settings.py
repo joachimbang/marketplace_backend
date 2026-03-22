3ef82739ea071d50b299d8c6dd30a83419ecb635
unknown <joachimbangirahe.jb@gmail.com>
2026-03-22 22:02:54 +0200

    Update REST framework settings to include default permission classes

--- config/config/settings.py
+++ config/config/settings.py
@@ -34,8 +34,11 @@ ALLOWED_HOSTS = []
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
 
