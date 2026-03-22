201be2d13b29b56c97458299fafa9ef3823bb4ed
unknown <joachimbangirahe.jb@gmail.com>
2026-03-22 20:18:55 +0200

    Add user selectors for retrieving user data by ID, email, and all users

---
+++ config/core_selectors/user_selectors.py
@@ -0,0 +1,16 @@
+from accounts.models import User
+
+
+def get_user_by_id(user_id):
+
+    return User.objects.filter(id=user_id).first()
+
+
+def get_user_by_email(email):
+
+    return User.objects.filter(email=email).first()
+
+
+def get_all_users():
+
+    return User.objects.all()
