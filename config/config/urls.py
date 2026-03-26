3680f303ff198106d111f9fddef83af21e44cac5
unknown <joachimbangirahe.jb@gmail.com>
2026-03-26 22:29:36 +0200

    chore(config): update main urls to include products and media

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,10 +1,15 @@
-from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
 
-    path("admin/", admin.site.urls),
+    path("api/accounts/", include("accounts.urls")),
 
-    path("api/", include("accounts.urls")),
+    path("api/products/", include("products.urls")),
 
 ]
+
+# Cette ligne permet d'accéder aux images via ton navigateur ou ton app mobile
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
