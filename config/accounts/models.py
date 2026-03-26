a89e29bc2f549270674c33b1470532b774a950e1
unknown <joachimbangirahe.jb@gmail.com>
2026-03-26 22:29:27 +0200

    refactor(accounts): update user model role and properties

--- config/accounts/models.py
+++ config/accounts/models.py
@@ -31,19 +31,36 @@ class User(AbstractBaseUser, PermissionsMixin):
         choices=ROLE_CHOICES,
     )
 
-    is_active = models.BooleanField(default=True)
-
-    is_staff = models.BooleanField(default=False)
-
     is_verified = models.BooleanField(default=False)
 
     date_joined = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
-
+    # Set email as the unique identifier for authentication instead of username
     USERNAME_FIELD = "email"
-
+    # Make username a required field when creating a user
     REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
         return self.email
+
+    @property
+    def is_active(self):
+        return True # Nécessaire pour que le token soit reconnu
+
+    @property
+    def is_admin(self):
+        return self.role == "admin"
+
+    @property
+    def is_seller(self):
+        return self.role == "seller"
+
+    @property
+    def is_buyer(self):
+        return self.role == "buyer"
+
+    # Pour la compatibilité interne Django (Admin/Permissions)
+    @property
+    def is_staff(self):
+        return self.is_admin
